f404c805a2e6f9e5251408cb31c30d2e23865da8
FeiyiWang <rephycca@gmail.com>
2020-03-19 21:37:38 -0400

    Add translation button

--- static/js/matomo.js
+++ static/js/matomo.js
@@ -11,6 +11,6 @@ _paq.push(['enableLinkTracking']);
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/rephycca.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://cdn.matomo.cloud/rephycca.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
 })();
 <!-- End Matomo Code -->
