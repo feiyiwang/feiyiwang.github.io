5ef0f2ac32f547a9a2a165ed7ef1071e4ea3bfa3
FeiyiWang <rephycca@gmail.com>
2020-04-05 23:48:47 -0400

    fix header and footer

---
+++ static/js/template.js
@@ -0,0 +1,5 @@
+/**
+ * Created by feiyiwang on 4/5/20.
+ */
+var foot = '<p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">FEIYI WANG&nbsp;&nbsp;|&nbsp;&nbsp;+1 (508)873-7966&nbsp;&nbsp;&nbsp;&nbsp;REPHYCCA@GMAIL.COM&nbsp;&nbsp;|&nbsp;&nbsp;PRIVACY POLICY&nbsp;&nbsp;&nbsp;&nbsp;TERMS + CONDITIONS <span style="float: right"> © 2020 FEIYI WANG All Rights Reserved.</span> </p>';
+var head_in_folder = "<a class='navbar-brand' href='../index.html'>Fey斐's Little Home</a> <img src='../icon/logo_in_folder.png' alt='Logo' style='width:190px;height:50px;padding-top:3px;float: right'>";
