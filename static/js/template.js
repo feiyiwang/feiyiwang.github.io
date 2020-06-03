301adcb9d61153d92f1e03380cb5a1ccbf2384ac
FeiyiWang <rephycca@gmail.com>
2020-06-03 19:12:09 -0400

    change email

--- static/js/template.js
+++ static/js/template.js
@@ -1,5 +1,5 @@
 /**
  * Created by feiyiwang on 4/5/20.
  */
-var foot = '<p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">FEIYI WANG &nbsp;|&nbsp; +1 (508)873-7966 &nbsp;&nbsp; REPHYCCA@GMAIL.COM &nbsp;|&nbsp; PRIVACY POLICY &nbsp;&nbsp; TERMS + CONDITIONS <span style="float: right"> © 2020 FEIYI WANG All Rights Reserved.</span> </p>';
+var foot = '<p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">FEIYI WANG &nbsp;|&nbsp; +1 (508)873-7966 &nbsp;&nbsp; FEIYIWANG1201@GMAIL.COM &nbsp;|&nbsp; PRIVACY POLICY &nbsp;&nbsp; TERMS + CONDITIONS <span style="float: right"> © 2020 FEIYI WANG All Rights Reserved.</span> </p>';
 var head_in_folder = "<button onclick=window.history.back() title='Go back' class='navbar-brand goBack' style='padding: 10px 15px ; height:auto;'>Fey斐's Little Home</button> <img src='../icon/logo_in_folder.png' alt='Logo' style='width:150px;height:40px;padding-top:3px;float: right;margin-right: 15px;'>";
