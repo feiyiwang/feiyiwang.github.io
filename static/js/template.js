db07fd5670f0a3c1e2958d8203299b00095e396e
feiyiwang <rephycca@gmail.com>
2025-12-19 22:10:59 -0500

    update profile 2025.12

--- static/js/template.js
+++ static/js/template.js
@@ -1,5 +1,52 @@
 /**
  * Created by feiyiwang on 4/5/20.
  */
-var foot = '<p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">FEIYI WANG &nbsp;|&nbsp; +1 (508)873-7966 &nbsp;&nbsp; FEIYIWANG1201@GMAIL.COM &nbsp;|&nbsp; PRIVACY POLICY &nbsp;&nbsp; TERMS + CONDITIONS <span style="float: right"> © 2020 FEIYI WANG All Rights Reserved.</span> </p>';
-var head_in_folder = "<button onclick=window.history.back() title='Go back' class='navbar-brand goBack' style='padding: 10px 15px ; height:auto;'>Fey斐's Little Home</button> <img src='../icon/logo_in_folder.png' alt='Logo' style='width:150px;height:40px;padding-top:3px;float: right;margin-right: 15px;'>";
+var foot = `
+<p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">Contact me: &nbsp;
+<a href="mailto:feiyi.wang@helsinki.fi" class="icon-link" aria-label="Send email" target="_blank" rel="noopener">
+  <span class="inline-hover-swap">
+    <img src="icon/email_grey.png" class="base-img" alt="Email">
+    <img src="icon/email.png" class="hover-img" alt="">
+  </span>
+</a> &nbsp;&nbsp;Email &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://scholar.google.com/citations?user=nC5bGNEAAAAJ&hl=en&oi=ao" class="icon-link" target="_blank" rel="noopener">
+  <span class="inline-hover-swap">
+    <img src="icon/google_grey.png" class="base-img" alt="">
+    <img src="icon/google.png" class="hover-img" alt="">
+  </span> 
+</a> &nbsp;&nbsp;Google Scholar &nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="https://www.linkedin.com/in/feiyi-wang-fey/" class="icon-link" target="_blank" rel="noopener">
+  <span class="inline-hover-swap">
+    <img src="icon/linkedin_grey.png" class="base-img" alt="">
+    <img src="icon/linkedin.png" class="hover-img" alt="">
+  </span>
+</a> &nbsp;&nbsp;LinkedIn &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://github.com/feiyiwang" class="icon-link" target="_blank" rel="noopener">
+  <span class="inline-hover-swap">
+    <img src="icon/github_grey.png" class="base-img" alt="">
+    <img src="icon/github.png" class="hover-img" alt="">
+  </span>
+</a> &nbsp;&nbsp;GitHub &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://x.com/feiyi_wang" class="icon-link" target="_blank" rel="noopener">
+  <span class="inline-hover-swap">
+    <img src="icon/x_grey.png" class="base-img" alt="">
+    <img src="icon/x.png" class="hover-img" alt="">
+  </span>
+</a> &nbsp;&nbsp;X / twitter &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://orcid.org/0000-0002-8179-488X" class="icon-link" target="_blank" rel="noopener">
+  <span class="inline-hover-swap">
+    <img src="icon/orcid_grey.png" class="base-img" alt="">
+    <img src="icon/orcid.png" class="hover-img" alt="">
+  </span>
+</a> &nbsp;&nbsp;ORCID &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://www.youtube.com/@feiyi_wang" class="icon-link" target="_blank" rel="noopener">
+  <span class="inline-hover-swap">
+    <img src="icon/youtube_grey.png" class="base-img" alt="">
+    <img src="icon/youtube.png" class="hover-img" alt="">
+  </span>
+</a> &nbsp;&nbsp;YouTube
+<span style="float: right"> © 2025 FEIYI WANG All Rights Reserved.</span> </p>
+`;
+    
+    // '<p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">FEIYI WANG &nbsp;|&nbsp; <span class="inline-hover-swap"><img src="icon/email_grey.png" class="base-img" alt=""><img src="icon/email.png" class="hover-img" alt=""></span>&nbsp;: FEIYI.WANG@HELSINKI.FI &nbsp;&nbsp; <span class="inline-hover-swap"><img src="icon/email_grey.png" class="base-img" alt=""><img src="icon/email.png" class="hover-img" alt=""></span>&nbsp;: FEIYI.WANG@HELSINKI.FI &nbsp;|&nbsp; PRIVACY POLICY &nbsp;&nbsp; TERMS + CONDITIONS <span style="float: right"> © 2025 FEIYI WANG All Rights Reserved.</span> </p>';
+var head_in_folder = "<button onclick=window.history.back() title='Go back' class='navbar-brand goBack' style='padding: 10px 15px ; height:auto;'>Feiyi Wang 王斐艺&nbsp;&nbsp;</button> <img src='../icon/logo_in_folder.png' alt='Logo' style='width:150px;height:40px;padding-top:3px;float: right;margin-right: 15px;'>";
