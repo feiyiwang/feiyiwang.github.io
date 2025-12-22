1e8fe52482966843b44e8be70a7323fa251f7fce
feiyiwang <rephycca@gmail.com>
2025-12-21 20:38:59 -0500

    fix minor bug

--- static/js/template.js
+++ static/js/template.js
@@ -49,4 +49,7 @@ var foot = `
 `;
     
     // '<p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">FEIYI WANG &nbsp;|&nbsp; <span class="inline-hover-swap"><img src="icon/email_grey.png" class="base-img" alt=""><img src="icon/email.png" class="hover-img" alt=""></span>&nbsp;: FEIYI.WANG@HELSINKI.FI &nbsp;&nbsp; <span class="inline-hover-swap"><img src="icon/email_grey.png" class="base-img" alt=""><img src="icon/email.png" class="hover-img" alt=""></span>&nbsp;: FEIYI.WANG@HELSINKI.FI &nbsp;|&nbsp; PRIVACY POLICY &nbsp;&nbsp; TERMS + CONDITIONS <span style="float: right"> © 2025 FEIYI WANG All Rights Reserved.</span> </p>';
-var head_in_folder = "<button onclick=window.history.back() title='Go back' class='navbar-brand goBack' style='padding: 10px 15px ; height:auto;'>Feiyi Wang 王斐艺&nbsp;&nbsp;</button> <img src='../icon/logo_in_folder.png' alt='Logo' style='width:150px;height:40px;padding-top:3px;float: right;margin-right: 15px;'>";
+var head_in_folder = `
+<button onclick=window.history.back() title='Go back' class='navbar-brand goBack' style='padding: 10px 15px ; height:auto;'>Feiyi Wang 王斐艺&nbsp;&nbsp;</button>
+<img src='../icon/logo_in_folder.png' alt='Logo' style='width:36px;height:40px;padding-top:3px;float: right;margin-right: 15px;'>
+`;
