aa698d57f8578424eeae8004cf96bae7ac49125a
feiyiwang <rephycca@gmail.com>
2025-12-27 11:43:33 -0500

    update 2025.12

--- static/js/template.js
+++ static/js/template.js
@@ -3,48 +3,48 @@
  */
 var foot = `
 <p class="sans grey" style="margin: 2px 5% 0 5%;font-size: 12px">Contact me: &nbsp;
-<a href="mailto:feiyi.wang@helsinki.fi" class="icon-link" aria-label="Send email" target="_blank" rel="noopener">
+<a href="mailto:feiyi.wang@helsinki.fi" class="icon-link text-link" aria-label="Send email" target="_blank" rel="noopener">
   <span class="inline-hover-swap">
     <img src="../icon/email_grey.png" class="base-img" alt="Email">
     <img src="../icon/email.png" class="hover-img" alt="">
-  </span>
-</a> &nbsp;&nbsp;Email &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="https://scholar.google.com/citations?user=nC5bGNEAAAAJ&hl=en&oi=ao" class="icon-link" target="_blank" rel="noopener">
+  </span>&nbsp;&nbsp;Email
+</a>  &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://scholar.google.com/citations?user=nC5bGNEAAAAJ&hl=en&oi=ao" class="icon-link text-link" target="_blank" rel="noopener">
   <span class="inline-hover-swap">
     <img src="../icon/google_grey.png" class="base-img" alt="">
     <img src="../icon/google.png" class="hover-img" alt="">
-  </span> 
-</a> &nbsp;&nbsp;Google Scholar &nbsp;&nbsp;|&nbsp;&nbsp;
-<a href="https://www.linkedin.com/in/feiyi-wang-fey/" class="icon-link" target="_blank" rel="noopener">
+  </span> &nbsp;&nbsp;Google Scholar
+</a>  &nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="https://www.linkedin.com/in/feiyi-wang-fey/" class="icon-link text-link" target="_blank" rel="noopener">
   <span class="inline-hover-swap">
     <img src="../icon/linkedin_grey.png" class="base-img" alt="">
     <img src="../icon/linkedin.png" class="hover-img" alt="">
-  </span>
-</a> &nbsp;&nbsp;LinkedIn &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="https://github.com/feiyiwang" class="icon-link" target="_blank" rel="noopener">
+  </span>&nbsp;&nbsp;LinkedIn
+</a>  &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://github.com/feiyiwang" class="icon-link text-link" target="_blank" rel="noopener">
   <span class="inline-hover-swap">
     <img src="../icon/github_grey.png" class="base-img" alt="">
     <img src="../icon/github.png" class="hover-img" alt="">
-  </span>
-</a> &nbsp;&nbsp;GitHub &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="https://x.com/feiyi_wang" class="icon-link" target="_blank" rel="noopener">
+  </span>&nbsp;&nbsp;GitHub
+</a>  &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://x.com/feiyi_wang" class="icon-link text-link" target="_blank" rel="noopener">
   <span class="inline-hover-swap">
     <img src="../icon/x_grey.png" class="base-img" alt="">
     <img src="../icon/x.png" class="hover-img" alt="">
-  </span>
-</a> &nbsp;&nbsp;X / twitter &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="https://orcid.org/0000-0002-8179-488X" class="icon-link" target="_blank" rel="noopener">
+  </span>&nbsp;&nbsp;X / twitter
+</a>  &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://orcid.org/0000-0002-8179-488X" class="icon-link text-link" target="_blank" rel="noopener">
   <span class="inline-hover-swap">
     <img src="../icon/orcid_grey.png" class="base-img" alt="">
     <img src="../icon/orcid.png" class="hover-img" alt="">
-  </span>
-</a> &nbsp;&nbsp;ORCID &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="https://www.youtube.com/@feiyi_wang" class="icon-link" target="_blank" rel="noopener">
+  </span>&nbsp;&nbsp;ORCID
+</a> &nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="https://www.youtube.com/@feiyi_wang" class="icon-link text-link" target="_blank" rel="noopener">
   <span class="inline-hover-swap">
     <img src="../icon/youtube_grey.png" class="base-img" alt="">
     <img src="../icon/youtube.png" class="hover-img" alt="">
-  </span>
-</a> &nbsp;&nbsp;YouTube
+  </span>&nbsp;&nbsp;YouTube
+</a> 
 <span style="float: right"> © 2025 FEIYI WANG All Rights Reserved.</span> </p>
 `;
     
